b6975a5992067f00d4b69431fe000e579932b855
Lorenzo <154640621+lorenzoNeedyou@users.noreply.github.com>
2024-05-22 12:33:35 +0000

    Fixed Bug..

--- script/bye.js
+++ script/bye.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("Bye")==0 || (event.body.indexOf("bye")==0)) {
     var msg = {
-        body: "Bye, Love see You soon 😄",
+        body: "See You Soon ${name} .",
         attachment: fs.createReadStream(__dirname + `/noprefix/bye.gif`)
       }
       api.sendMessage(msg, threadID, messageID);
