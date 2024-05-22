433935b079212fbb2da7a868d98dc89099a3d680
Lorenzo <154640621+lorenzoNeedyou@users.noreply.github.com>
2024-05-22 11:37:47 +0000

    Added bye cmd not prefix...👋

---
+++ script/bye.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "bye",
+    version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Lorenzo",
+  hasPrefix: false,
+  description: "not prefix and not command",
+  shortCategory: "not command ",
+  commandCategory: "noprefix",
+  usages: "bye",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("Bye")==0 || (event.body.indexOf("bye")==0)) {
+    var msg = {
+        body: "Bye, Love see You soon 😄",
+        attachment: fs.createReadStream(__dirname + `/noprefix/bye.gif`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("👋", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
