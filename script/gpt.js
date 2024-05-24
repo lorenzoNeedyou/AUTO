7902b5978babeed2b03b088f3abcb8426ca36956
Lorenzo <154640621+lorenzoNeedyou@users.noreply.github.com>
2024-05-24 09:41:12 +0000

    added educ cmd

---
+++ script/gpt.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+module.exports.config = {
+  name: 'gpt',
+  hasPermssion: 0,
+  credits: "𝐌𝐀𝐑𝐉𝐇𝐔𝐍 𝐁𝐀𝐘𝐋𝐎𝐍",
+  description: '𝙴𝙳𝚄𝙲𝙰𝚃𝙸𝙾𝙽𝙰𝙻 𝙰𝙸 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 ',
+  hasPrefix: false,
+  commandCategory: 'educational',
+  usages: '𝙰𝙸 - 𝚈𝙾𝚄𝚁 𝚀𝚄𝙴𝚂𝚃𝙸𝙾𝙽 ',
+  cooldown: 2,
+};
+module.exports.run = async function({ api, event, args }) {
+  try {
+    const prompt = encodeURIComponent(args.join(" "));
+    if (!prompt) return api.sendMessage("🤖 Please enter a prompt!!!", event.threadID, event.messageID);
+    api.sendMessage("Processing your question.", event.threadID, event.messageID);
+    const apiUrl = "https://boxgptapi.replit.app/api/chatgpt?msg=";
+    const response = await axios.get(apiUrl + prompt);
+    const responseData = response.data.message;
+
+    await api.sendMessage(`${responseData}`, event.threadID);
+  } catch (error) {
+    console.error(error);
+    return api.sendMessage(error.message, event.threadID, event.messageID);
+  }
+};
